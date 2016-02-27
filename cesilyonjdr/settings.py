ba53c65ec5b2e38cba16afeb22670f5459a9e571
EpicKiwi <lelycandu76@gmail.com>
2016-02-27 09:19:26 +0100

    Desactivation debug

--- cesilyonjdr/settings.py
+++ cesilyonjdr/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hb#x*3pm#pzp6w2e@izc#y0pg9wtqzq-av+-knb^b)=n&$5(rc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
