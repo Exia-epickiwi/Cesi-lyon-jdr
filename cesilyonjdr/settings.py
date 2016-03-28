be701a47e7a61996c6cbaf6532f54a0a658597d4
EpicKiwi <lelycandu76@gmail.com>
2016-03-28 14:10:49 +0200

    Hotfix line-height markdown

--- cesilyonjdr/settings.py
+++ cesilyonjdr/settings.py
@@ -95,9 +95,9 @@ WSGI_APPLICATION = 'cesilyonjdr.wsgi.application'
 
 DATABASES = {
 #    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cesilyonjdr',
