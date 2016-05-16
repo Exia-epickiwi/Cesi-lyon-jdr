f7f605f05e55e98ad79f862245695a45e4a60a39
EpicKiwi <lelycandu76@gmail.com>
2016-05-16 12:03:08 +0200

    Header actif

--- static/js/global.js
+++ static/js/global.js
@@ -11,4 +11,51 @@ $(function(){
            },500)
        },5000);
    });
+
+    var activeHeader = true;
+
+    var $header = $("header");
+    $("body").css("padding-top",$header.innerHeight());
+    if($(window).width() < 600){
+        activeHeader = false;
+    }
+    $(window).on('resize',function(e){
+        $("body").css("padding-top",$header.innerHeight());
+        if($(window).width() < 600){
+            activeHeader = false;
+            $header.css("top",0+"px");
+        } else {
+            activeHeader = true;
+        }
+    });
+
+    var lastScroll = $(window).scrollTop();
+    $(window).on('scroll',function(e){
+        if(activeHeader) {
+            var thisScroll = $(window).scrollTop();
+            var lastTop = $header.css("top");
+            lastTop = parseInt(lastTop.slice(0, lastTop.length - 2));
+            var newTop = 0;
+
+            if (thisScroll > lastScroll) {
+                newTop = lastTop + (lastScroll - thisScroll);
+                if (newTop <= -($header.innerHeight() + 30)) {
+                    newTop = -($header.innerHeight() + 30);
+                }
+            } else if (thisScroll < lastScroll) {
+                newTop = lastTop - (thisScroll - lastScroll);
+                if(thisScroll < $header.innerHeight()){
+                    if (newTop >= 0) {
+                        newTop = 0;
+                    }
+                } else {
+                    if (newTop >= -$header.find("h1").innerHeight()) {
+                        newTop = -$header.find("h1").innerHeight();
+                    }
+                }
+            }
+            lastScroll = thisScroll;
+            $header.css("top",newTop+"px");
+        }
+    });
 });
